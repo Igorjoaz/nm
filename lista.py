7aa249717f270c2d8cb26f30db61cf249ae83ac8
Elison12 <56695160+Elison12@users.noreply.github.com>
2019-10-17 20:35:35 -0300

    Update lista.py

--- lista.py
+++ lista.py
@@ -16,6 +16,27 @@ print('Sua média é: {:.1f}'.format(media))
 ##terceira questao
 A = float(input("Digite a primeira nota: "))
 B = float(input("Digite a segunda: "))
-
 media = ((A*3.5)+(B*7.5))/2
 print('media = {:5f}'.format(media))
+
+##quarta questao
+srt = int(input('try your luck:'))
+print('O numero sorteado foi', srt)
+
+##quinta questao
+num = int(input('insira um numero:'))
+print(num-1,num+1)
+
+##sexta questao
+A = int(input())
+B = int(input())
+X = A + B
+print('X =',X)
+
+##nona questao
+print('RESTAURANTE COMA BEM, BOM DIA!!')
+preço = float(input("Digite os gastos: "))
+gorgeta = 0.1*preço
+total = preço + gorgeta
+print('o valor a pagar é',total)
+
