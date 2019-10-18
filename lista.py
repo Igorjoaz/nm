679a707a77cfea902b40f738f38e934c73c55070
Igorjoaz <56703574+Igorjoaz@users.noreply.github.com>
2019-10-17 23:37:00 -0300

    Update lista.py

--- lista.py
+++ lista.py
@@ -1,42 +1,65 @@
-## primeira questao
+## TABUADA
 x = int(input())
-n = 1                                      
-while (n<10):
+n = 1                                      * LEMBRE-SE QUE A SAÍDA TEM QUE SER IGUAL A DO HUXLEY (CASO CONTRÁRIO NÃO ACEITA)
+while (n<10):                              
     print(x,'X',n,'=',n*x)
     n = n + 1
     
-## segunda questao
-A = float(input("Digite a primeira nota: "))
-B = float(input("Digite a segunda: "))
-C = float(input("A terceira: "))
+## MEDIA 2 
+A = float(input())
+B = float(input())
+C = float(input())
 
 media = ((A*2)+(B*3)+(C*5))/10
-print('Sua média é: {:.1f}'.format(media))
+print('MEDIA = {:.1f}'.format(media))   ou print('MEDIA = %.1f'%media)
 
-##terceira questao
-A = float(input("Digite a primeira nota: "))
-B = float(input("Digite a segunda: "))
+## MEDIA 1 
+A = float(input())
+B = float(input())
 media = ((A*3.5)+(B*7.5))/11
-print('media = {:.5f}'.format(media))
+print('MEDIA = {:.5f}'.format(media)) ou print('MEDIA = %.5f'%media)
 
-##quarta questao
-srt = int(input('try your luck:'))
-print('O numero sorteado foi', srt)
+## BINGO 
+x = int(input())
+print('O numero sorteado foi:',x)
 
-##quinta questao
-num = int(input('insira um numero:'))
+## ANTECESSOR E SUCESSOR 
+num = int(input())
 print(num-1,num+1)
 
-##sexta questao
+## EXTREMAMENTE BASICO 
 A = int(input())
 B = int(input())
-X = A + B
-print('X =',X)
+x = A + B
+print('X =',x)
 
-##nona questao
-print('RESTAURANTE COMA BEM, BOM DIA!!')
-preço = float(input("Digite os gastos: "))
-gorgeta = 0.1*preço
-total = preço + gorgeta
-print('o valor a pagar é',total)
+## COMABEM 
+gasto = float(input())
+total = (gasto * 0.1) + gasto 
+print('{:.2f}'.format(total)) 
+
+## AREA DO CIRCULO 
+raio = float(input())
+pi = 3.14159 
+raio = (raio)/100   ( para transformar de cm em metro)
+area = (raio**2)* pi
+print('Area = {:.4f}'.format(area))
+
+## DIFERENCA
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+x = A * B
+y = C * D 
+print('DIFERENCA =',x - y)
+
+##SALARIO 
+numero          = int(input())
+trabalho_hrs    = int(input())
+recebe_por_hora = float(input())
+salario = trabalho_hrs * recebe_por_hora
+print('NUMBER =',numero)
+print('SALARY = R$ %.2f'%salario)
+
 
