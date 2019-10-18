0bc7c61a8305afddfe8a64607ef4db7f1af3e76c
Igorjoaz <56703574+Igorjoaz@users.noreply.github.com>
2019-10-17 21:11:37 -0300

    Update lista.py

--- lista.py
+++ lista.py
@@ -10,13 +10,13 @@ A = float(input("Digite a primeira nota: "))
 B = float(input("Digite a segunda: "))
 C = float(input("A terceira: "))
 
-media = ((A*2)+(B*3)+(C*5))/3
+media = ((A*2)+(B*3)+(C*5))/10
 print('Sua média é: {:.1f}'.format(media))
 
 ##terceira questao
 A = float(input("Digite a primeira nota: "))
 B = float(input("Digite a segunda: "))
-media = ((A*3.5)+(B*7.5))/2
+media = ((A*3.5)+(B*7.5))/11
 print('media = {:.5f}'.format(media))
 
 ##quarta questao
