d94998144fb901338222929b30a5885c15fbf72f
Igorjoaz <56703574+Igorjoaz@users.noreply.github.com>
2019-10-22 15:44:45 -0300

    Update lista.py

--- lista.py
+++ lista.py
@@ -5,6 +5,19 @@ while (n<10):
     print(x,'X',n,'=',n*x)
     n = n + 1
     
+ # TABUADA ( COMO PROFESSOR PEDIU ) 
+n = int(input())
+print(n,'X 1 =', n*1)
+print(n,'X 2 =', n*2)
+print(n,'X 3 =', n*3)
+print(n,'X 4 =', n*4)
+print(n,'X 5 =', n*5)
+print(n,'X 6 =', n*6)
+print(n,'X 7 =', n*7)
+print(n,'X 8 =', n*8)
+print(n,'X 9 =', n*9)
+
+    
 ## MEDIA 2 
 A = float(input())
 B = float(input())
